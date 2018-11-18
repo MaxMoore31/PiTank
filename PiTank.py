b84f1773744980646476394ab53b98ba02c1a752
Max Moore <max.moore31@hotmail.com>
2018-11-18 15:06:59 +1030

    driving added

--- PiTank.py
+++ PiTank.py
@@ -1,29 +1,74 @@
 import RPi.GPIO as GPIO
 import time
 from pynput import keyboard
+from pynput.keyboard import Listener
+from inputs import devices
 
-def on_press(key):
-    if key == keyboard.key.left:
-        print("left")
-    if key == keyboard.key.right:
-        print("right")
-        
-
-
-
+#init variables
+msPerCycle = 1000/50
+fullStick = 2.5
+deadStick = 0.5
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(7, GPIO.OUT)
-p = GPIO.PWM(7, 50)
-p.start(50)
-leftPos = 0.75
-rightPos = 2.5
-middlePos = (rightPos - leftPos)/2+leftPos
-posList = [leftPos, middlePos, rightPos, middlePos]
-msPerCycle = 1000/50
+l = GPIO.PWM(7, 50)
+GPIO.setup(40, GPIO.OUT)
+r = GPIO.PWM(40, 50)
+
+#begin tank at idle
+dutyCyclePercentage = deadStick * 100/msPerCycle
+l.start(dutyCyclePercentage)
+dutyCyclePercentage = deadStick * 100/msPerCycle
+r.start(dutyCyclePercentage)
+
+def leftTreadOn():
+    dutyCyclePercentage = fullStick * 100/msPerCycle
+    l.start(dutyCyclePercentage)
+def rightTreadOn():
+    dutyCyclePercentage = fullStick * 100/msPerCycle
+    r.start(dutyCyclePercentage)
+    
+def leftTreadOff():
+    dutyCyclePercentage = deadStick * 100/msPerCycle
+    l.start(dutyCyclePercentage)
+def rightTreadOff():
+    dutyCyclePercentage = deadStick * 100/msPerCycle
+    r.start(dutyCyclePercentage)
+
+
+def on_press(key):
+    if key == keyboard.Key.left:
+        print("left")
+        leftTreadOn()
+    if key == keyboard.Key.right:
+        print("right")
+        rightTreadOn()
+        
+def on_release(key):
+    #print("{0} I ".format(str(key)))
+    if key == keyboard.Key.left:
+        print("left")
+        leftTreadOff()
+    if key == keyboard.Key.right:
+        print("right")
+        rightTreadOff()
+
+
+
+#leftPos = 0.75
+#rightPos = 2.5
+#middlePos = (rightPos - leftPos)/2+leftPos
+#posList = [leftPos, middlePos, rightPos, middlePos]
+
+
 try:
+    with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+        listener.join()
     print("standby")
-    time.sleep(5)
+    while True:
+        time.sleep(0.1)
         
 
     
