ebc59a0a298c39e30520c1849c548181af180659
max <max.moore31@hotmail.com>
2018-12-09 18:56:30 +1100

    socketio

---
+++ client3.py
@@ -0,0 +1,24 @@
+from socketIO_client import SocketIO, LoggingNamespace
+
+def on_connect():
+    print('connect')
+
+def on_disconnect():
+    print('disconnect')
+
+def on_reconnect():
+    print('reconnect')
+
+def on_aaa_response(*args):
+    print('on_aaa_response', args)
+
+socketIO = SocketIO('192.168.1.2', 8000, LoggingNamespace)
+socketIO.on('connect', on_connect)
+socketIO.on('disconnect', on_disconnect)
+socketIO.on('reconnect', on_reconnect)
+
+# Listen
+socketIO.on('aaa_response', on_aaa_response)
+socketIO.emit('aaa')
+socketIO.emit('aaa')
+socketIO.wait(seconds=1)
